f65db5e59fe0e59e7591a3c5e801ea32525b7e7a
duddanobre <dudarnobre@gmail.com>
2021-02-17 20:09:44 -0300

    Aplicação web para Ongs feito em React e Node JS

--- frontend/src/pages/Logon/Profile/index.js
+++ frontend/src/pages/Logon/Profile/index.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import '../Profile/style.css';
 import logoIMG from '../../../assets/logo.svg';
 
+import api from '../../../services/api';
+
 export default function Profile(){
+
+const history = useHistory();
+const [incidents, setIncidents] = useState([]);
 const ongName = localStorage.getItem('ongName');
+const ongId = localStorage.getItem('ongId');
+
+useEffect(() => {
+    api.get('profile', {
+        headers: {
+            Authorization: ongId,
+        }
+    }).then(response => {
+        setIncidents(response.data)
+    })
+}, [ongId]);
+
+async function handleDeleIncident(id){
+    try {
+        await api.delete(`incidents/${id}`, {
+            headers: {
+                Authorization: ongId,
+            }
+        });
+
+        setIncidents(incidents.filter(incident => incident.id !== id));
+    } catch (error) {
+        alert('Falha ao deletar, tente novamente');
+    }
+}
+
+function handleLogout(){
+    localStorage.clear();
+
+    history.push('/');
+}
 
     return(
         <div className="profile-container">
@@ -15,7 +51,7 @@ const ongName = localStorage.getItem('ongName');
                 <span>Bem vinda, {ongName}!</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button" >
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="E02041" />
                 </button>
             </header>
@@ -23,81 +59,20 @@ const ongName = localStorage.getItem('ongName');
             <h1>CASOS CADASTRADOS</h1>
 
             <ul>
-                <li>
-                    <strong>CASO</strong>
-                    <p>Caso I</p>
+                {incidents.map(incident => (
+                    <li key={incident.id}>
+                    <strong>CASO:</strong>
+                    <p>{incident.titulo}</p>
                     <strong>DESCRIÇÃO</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur
-                        adipisicing elit. Repudiandae pariatur minima
-                        vel laborum. Alias ab quo eius, et consequatur corrupti
-                        nemo delectus dicta, odio, corporis inventore repudiandae eum exercitationem debitis!</p>
+                    <p>{incident.descricao}</p>
                     <strong>VALOR</strong>
-                    <p>550,00</p>
+                    <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.valor)}</p>
 
-                <button type="button">
+                <button onClick={() => handleDeleIncident(incident.id)} type="button">
                     <FiTrash2 size={20} color="#E02041" />
                 </button>
                 </li>
-                <li>
-                    <strong>CASO</strong>
-                    <p>Caso I</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur
-                        adipisicing elit. Repudiandae pariatur minima
-                        vel laborum. Alias ab quo eius, et consequatur corrupti
-                        nemo delectus dicta, odio, corporis inventore repudiandae eum exercitationem debitis!</p>
-                    <strong>VALOR</strong>
-                    <p>550,00</p>
-
-                <button type="button">
-                    <FiTrash2 size={20} color="E02041" />
-                </button>
-                </li>
-                <li>
-                    <strong>CASO</strong>
-                    <p>Caso I</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur
-                        adipisicing elit. Repudiandae pariatur minima
-                        vel laborum. Alias ab quo eius, et consequatur corrupti
-                        nemo delectus dicta, odio, corporis inventore repudiandae eum exercitationem debitis!</p>
-                    <strong>VALOR</strong>
-                    <p>550,00</p>
-
-                <button type="button">
-                    <FiTrash2 size={20} color="E02041" />
-                </button>
-                </li>
-                <li>
-                    <strong>CASO</strong>
-                    <p>Caso I</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur
-                        adipisicing elit. Repudiandae pariatur minima
-                        vel laborum. Alias ab quo eius, et consequatur corrupti
-                        nemo delectus dicta, odio, corporis inventore repudiandae eum exercitationem debitis!</p>
-                    <strong>VALOR</strong>
-                    <p>550,00</p>
-
-                <button type="button">
-                    <FiTrash2 size={20} color="E02041" />
-                </button>
-                </li>
-                <li>
-                    <strong>CASO</strong>
-                    <p>Caso I</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Lorem ipsum dolor sit amet consectetur
-                        adipisicing elit. Repudiandae pariatur minima
-                        vel laborum. Alias ab quo eius, et consequatur corrupti
-                        nemo delectus dicta, odio, corporis inventore repudiandae eum exercitationem debitis!</p>
-                    <strong>VALOR</strong>
-                    <p>550,00</p>
-
-                <button type="button">
-                    <FiTrash2 size={20} color="E02041" />
-                </button>
-                </li>
+                ))}
             </ul>
 
         </div>
